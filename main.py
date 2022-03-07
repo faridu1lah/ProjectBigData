ffc82e56958e218fde7fb3cfe72e849310abdd3e
Herish Ibrahim <herish.ibrahim@hva.nl>
2022-03-07 19:01:47 +0100

    hallo

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from db import connection
+import streamlit as sl
+import pandas as pd
 
 if connection.is_connected():
     cursor = connection.cursor()
     cursor.execute("select database();")
     print("You're connected to database in main: ", cursor.fetchone())
+
+
+header = sl.container()
+dataset = sl.container()
+
+with header:
+    sl.title('Hallo Big Data')
+
+with dataset:
+    amsterdam_data = pd.read_csv('data/h-amsterdam.csv')
+
+    sl.write(amsterdam_data.head(100))
