de48e8dbb04868c3aaa07dbf1d7245eaafccb195
FaridullahKhorshid <faridullah.khorshid@gmail.com>
2022-03-14 07:32:09 +0100

    map added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from db import connection
 import streamlit as sl
 import pandas as pd
+import folium
 
 if connection.is_connected():
     cursor = connection.cursor()
@@ -12,9 +13,35 @@ header = sl.container()
 dataset = sl.container()
 
 with header:
-    sl.title('Hallo Big Data')
+    sl.title("Hallo Big Data")
+
+amsterdam_data = pd.read_csv("data/h-amsterdam.csv")
 
 with dataset:
-    amsterdam_data = pd.read_csv('data/h-amsterdam.csv')
-
     sl.write(amsterdam_data.head(100))
+
+map_data = amsterdam_data
+map_data.rename(columns={"Lat": "lat", "Lon": "lon"}, inplace=True)
+
+sl.map(map_data)
+
+# map = folium.Map(
+#     location=[
+#         map_data.lat.mean(),
+#         map_data.lon.mean(),
+#     ],
+#     zoom_start=14,
+#     control_scale=True,
+# )
+
+# m = folium.Map(
+#     location=[45.523, -122.675],
+#     zoom_start=2,
+#     tiles="https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=mytoken",
+#     attr="Mapbox attribution",
+# )
+
+# sl.write(m)
+
+
+# map.Choropleth(name="test", data=map_data)
