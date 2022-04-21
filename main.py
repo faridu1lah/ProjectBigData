a16aeeaa954df109d94cd9d4cc2d1d3af133d0e5
Herish Ibrahim <herish.ibrahim@hva.nl>
2022-04-21 14:32:57 +0200

    3 verschillende navbar kijken welke we mooi vinden

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
-from db import connection
+from numpy import nanvar
+#from db import connection
 import streamlit as sl
 import pandas as pd
-import folium
+#import folium
+import navbar as nav
 
-if connection.is_connected():
-    cursor = connection.cursor()
-    cursor.execute("select database();")
-    print("You're connected to database in main: ", cursor.fetchone())
+# if connection.is_connected():
+#     cursor = connection.cursor()
+#     cursor.execute("select database();")
+#     print("You're connected to database in main: ", cursor.fetchone())
 
 
 header = sl.container()
 dataset = sl.container()
-
-with header:
-    sl.title("Hallo Big Data")
-
+# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
+EXAMPLE_NO = 1
 amsterdam_data = pd.read_csv("data/h-amsterdam.csv")
+map_data = amsterdam_data
+
+# with header:
+#     sl.title("Hallo Big Data")
+selected = nav.streamlit_menu(example=EXAMPLE_NO)
+selected1= nav.streamlit_menu(example=2)
+selected2 = nav.streamlit_menu(example=3)
+
+if selected == "Home":
+    sl.title(f"You have selected {selected}")
+    map_data.rename(columns={"Lat": "lat", "Lon": "lon"}, inplace=True)
+
+    sl.map(map_data)
+if selected == "Projects":
+    sl.title(f"You have selected {selected1}")
+    ch_data = amsterdam_data
+# ch_data["PriceRoom"] = ch_data["Price"] / 100 / ch_data["Room"]
+
+    chart_data = pd.DataFrame(amsterdam_data, columns=["Price", "Room"])
+
+    sl.line_chart(chart_data)
+
+if selected == "Contact":
+    sl.title(f"You have selected {selected2}")
+
+
 
 with dataset:
     sl.write(amsterdam_data.head(100))
 
-map_data = amsterdam_data
-map_data.rename(columns={"Lat": "lat", "Lon": "lon"}, inplace=True)
-
-sl.map(map_data)
-
-
-ch_data = amsterdam_data
-# ch_data["PriceRoom"] = ch_data["Price"] / 100 / ch_data["Room"]
-
-chart_data = pd.DataFrame(amsterdam_data, columns=["Price", "Room"])
-
-sl.line_chart(chart_data)
 
 # map = folium.Map(
 #     location=[
