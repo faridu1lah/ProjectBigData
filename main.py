478b09b47cc0d55cdad0fb60223840667d80f5bb
FaridullahKhorshid <faridullah.khorshid@gmail.com>
2022-05-10 23:19:38 +0200

    Adding navigation

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ st.markdown(
     """ 
     <style>
         .block-container {padding-bottom:1rem; }
-        iframe[title="streamlit_option_menu.option_menu"] {position:fixed; top:0;right: 0;left: 0;width: 100%;}
+        iframe[title="streamlit_option_menu.option_menu"] {position:fixed; top:0;right: 0;left: 0;width: 100%;z-index:9999999;}
         #MainMenu {visibility: hidden;}
         footer {visibility: hidden;}
     </style> 
@@ -35,7 +35,7 @@ selected = option_menu(
     menu_icon="cast",  # optional
     orientation="horizontal",
     styles={
-        "container": {"padding": "0 !important", "background-color": "#fafafa", "margin": "0", "max-width": "100%"},
+        "container": {"padding": "0 !important", "margin": "0", "max-width": "100%", "border-radius": "0"},
         "icon": {"color": "orange", "font-size": "25px"},
         "nav-item": {
             "flex-grow": "unset",
@@ -45,7 +45,7 @@ selected = option_menu(
             "font-size": "25px",
             "text-align": "left",
             "margin": "0px",
-            "--hover-color": "#eee",
+            # "--hover-color": "#eee",
         },
         "nav-link-selected": {"background-color": "#073045"},
     },
@@ -54,13 +54,11 @@ selected = option_menu(
 header = st.container()
 dataset = st.container()
 
-# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
-EXAMPLE_NO = 1
 amsterdam_data = pd.read_csv("data/h-amsterdam.csv")
 map_data = amsterdam_data
 
-# with header:
-#     st.title("Hallo Big Data")
+with header:
+    st.title("Hallo Big Data")
 # selected1 = nav.streamlit_menu(example=2)
 # selected2 = nav.streamlit_menu(example=3)
 
