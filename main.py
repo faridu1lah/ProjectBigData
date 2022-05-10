8eab8f481297c699151aae3f6b733cbd63e555e1
FaridullahKhorshid <faridullah.khorshid@gmail.com>
2022-05-10 23:03:37 +0200

    Adding navigation

--- main.py
+++ main.py
@@ -1,52 +1,91 @@
 from numpy import nanvar
-#from db import connection
-import streamlit as sl
+
+# from db import connection
+import streamlit as st
 import pandas as pd
-#import folium
-import navbar as nav
-from views import home,about,projecy
+
+# import folium
+from streamlit_option_menu import option_menu
+from views import prediction
 
 # if connection.is_connected():
 #     cursor = connection.cursor()
 #     cursor.execute("select database();")
 #     print("You're connected to database in main: ", cursor.fetchone())
 
+st.set_page_config(page_title="Big Data", layout="wide")
+# add css to document
+st.markdown(
+    """ 
+    <style>
+        .block-container {padding-bottom:1rem; }
+        iframe[title="streamlit_option_menu.option_menu"] {position:fixed; top:0;right: 0;left: 0;width: 100%;}
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+    </style> 
+""",
+    unsafe_allow_html=True,
+)
 
-header = sl.container()
-dataset = sl.container()
+# 2. horizontal menu with custom style
+selected = option_menu(
+    menu_title=None,  # required
+    options=["Home", "Prediction", "Contact"],  # required
+    icons=["house", "book", "envelope"],  # optional
+    menu_icon="cast",  # optional
+    orientation="horizontal",
+    styles={
+        "container": {"padding": "0 !important", "background-color": "#fafafa", "margin": "0", "max-width": "100%"},
+        "icon": {"color": "orange", "font-size": "25px"},
+        "nav-item": {
+            "flex-grow": "unset",
+            "flex-basis": "unset",
+        },
+        "nav-link": {
+            "font-size": "25px",
+            "text-align": "left",
+            "margin": "0px",
+            "--hover-color": "#eee",
+        },
+        "nav-link-selected": {"background-color": "#073045"},
+    },
+)
+
+header = st.container()
+dataset = st.container()
+
 # 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
 EXAMPLE_NO = 1
 amsterdam_data = pd.read_csv("data/h-amsterdam.csv")
 map_data = amsterdam_data
 
 # with header:
-#     sl.title("Hallo Big Data")
-selected = nav.streamlit_menu(example=EXAMPLE_NO)
-selected1= nav.streamlit_menu(example=2)
-selected2 = nav.streamlit_menu(example=3)
+#     st.title("Hallo Big Data")
+# selected1 = nav.streamlit_menu(example=2)
+# selected2 = nav.streamlit_menu(example=3)
 
 if selected == "Home":
-    home.load_view()
-    sl.title(f"You have selected {selected}")
+    # home.load_view()
+    st.title(f"You have selected {selected}")
     map_data.rename(columns={"Lat": "lat", "Lon": "lon"}, inplace=True)
 
-    sl.map(map_data)
-if selected == "Projects":
-    sl.title(f"You have selected {selected1}")
-    ch_data = amsterdam_data
-# ch_data["PriceRoom"] = ch_data["Price"] / 100 / ch_data["Room"]
+    st.map(map_data)
+if selected == "Prediction":
+    prediction.load_view()
+    # st.title(f"You have selected {selected}")
+    # ch_data = amsterdam_data
+    # # ch_data["PriceRoom"] = ch_data["Price"] / 100 / ch_data["Room"]
 
-    chart_data = pd.DataFrame(amsterdam_data, columns=["Price", "Room"])
+    # chart_data = pd.DataFrame(amsterdam_data, columns=["Price", "Room"])
 
-    sl.line_chart(chart_data)
+    # st.line_chart(chart_data)
 
 if selected == "Contact":
-    sl.title(f"You have selected {selected2}")
+    st.title(f"You have selected {selected}")
 
 
-
-with dataset:
-    sl.write(amsterdam_data.head(100))
+# with dataset:
+#     st.write(amsterdam_data.head(100))
 
 
 # map = folium.Map(
@@ -65,7 +104,7 @@ with dataset:
 #     attr="Mapbox attribution",
 # )
 
-# sl.write(m)
+# st.write(m)
 
 
 # map.Choropleth(name="test", data=map_data)
---
+++ views/prediction.py
@@ -0,0 +1,116 @@
+def load_view():
+    import streamlit as st
+
+    st.markdown("## Predicting house prices in Amsterdam")
+    with st.form(key="my_form"):
+
+        c1, c2 = st.columns([1, 5])
+
+        with c1:
+            ModelType = st.radio(
+                "Choose your model",
+                ["DistilBERT (Default)", "Flair"],
+                help="At present, you can choose between 2 models (Flair or DistilBERT) to embed your text. More to come!",
+            )
+
+            # if ModelType == "Default (DistilBERT)":
+            #     # kw_model = KeyBERT(model=roberta)
+
+            #     @st.cache(allow_output_mutation=True)
+            #     def load_model():
+            #         return KeyBERT(model=roberta)
+
+            #     kw_model = load_model()
+
+            # else:
+
+            #     @st.cache(allow_output_mutation=True)
+            #     def load_model():
+            #         return KeyBERT("distilbert-base-nli-mean-tokens")
+
+            #     kw_model = load_model()
+
+            top_N = st.slider(
+                "# of results",
+                min_value=1,
+                max_value=30,
+                value=10,
+                help="You can choose the number of keywords/keyphrases to display. Between 1 and 30, default number is 10.",
+            )
+            min_Ngrams = st.number_input(
+                "Minimum Ngram",
+                min_value=1,
+                max_value=4,
+                help="""The minimum value for the ngram range.
+                *Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases.
+                To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases.""",
+                # help="Minimum value for the keyphrase_ngram_range. keyphrase_ngram_range sets the length of the resulting keywords/keyphrases. To extract keyphrases, simply set keyphrase_ngram_range to (1, # 2) or higher depending on the number of words you would like in the resulting keyphrases.",
+            )
+
+            max_Ngrams = st.number_input(
+                "Maximum Ngram",
+                value=2,
+                min_value=1,
+                max_value=4,
+                help="""The maximum value for the keyphrase_ngram_range.
+                        *Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases.
+                        To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases.""",
+            )
+
+            StopWordsCheckbox = st.checkbox(
+                "Remove stop words",
+                help="Tick this box to remove stop words from the document (currently English only)",
+            )
+
+            use_MMR = st.checkbox(
+                "Use MMR",
+                value=True,
+                help="You can use Maximal Margin Relevance (MMR) to diversify the results. It creates keywords/keyphrases based on cosine similarity. Try high/low 'Diversity' settings below for interesting variations.",
+            )
+
+            Diversity = st.slider(
+                "Keyword diversity (MMR only)",
+                value=0.5,
+                min_value=0.0,
+                max_value=1.0,
+                step=0.1,
+                help="""The higher the setting, the more diverse the keywords.
+                
+                        Note that the *Keyword diversity* slider only works if the *MMR* checkbox is ticked.
+                     """,
+            )
+
+            submit_button = st.form_submit_button(label="✨ Predict data data!")
+
+        with c2:
+
+            st.markdown("### Check out the prices!")
+
+            import pandas as pd
+
+            amsterdam_data = pd.read_csv("data/h-amsterdam.csv")
+
+            amsterdam_data.rename(columns={"Lat": "lat", "Lon": "lon"}, inplace=True)
+
+            st.map(amsterdam_data)
+            # doc = st.text_area(
+            #     "Paste your text below (max 500 words)",
+            #     height=510,
+            # )
+
+            # MAX_WORDS = 500
+            # import re
+
+            # res = len(re.findall(r"\w+", doc))
+            # if res > MAX_WORDS:
+            #     st.warning(
+            #         "⚠️ Your text contains "
+            #         + str(res)
+            #         + " words."
+            #         + " Only the first 500 words will be reviewed. Stay tuned as increased allowance is coming! 😊"
+            #     )
+
+            #     doc = doc[:MAX_WORDS]
+
+    if not submit_button:
+        st.stop()
