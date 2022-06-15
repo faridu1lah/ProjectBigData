1ddfebcefd710bb461a74dd2d3cfae2eb8e6e6ea
FaridullahKhorshid <faridullah.khorshid@gmail.com>
2022-06-15 12:09:03 +0200

    packages and navigation

--- cleaning.py
+++ cleaning.py
@@ -72,14 +72,62 @@ def getData():
 
 
 def getGeoInfo():
-    from os import sep
+    # from os import sep
     import pandas as pd
+    import json
+    from shapely.validation import make_valid
+    from shapely.geometry import Polygon
 
     raw = pd.read_csv("data/lat_and_lon.csv", on_bad_lines="skip", sep=";")
+    jsonData = pd.read_json("https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=INDELING_WIJK&THEMA=gebiedsindeling")
 
-    raw.head(5)
+    pol = Polygon(
+        [
+            [
+                [4.8674055, 52.3717966],
+                [4.8695858, 52.368605],
+                [4.8708093, 52.3668503],
+                [4.8737991, 52.3677141],
+                [4.8768091, 52.3685032],
+                [4.8766794, 52.368677],
+                [4.8763027, 52.3689665],
+                [4.8759929, 52.3692536],
+                [4.8758061, 52.3694618],
+                [4.8756743, 52.3696392],
+                [4.8750449, 52.3706832],
+                [4.8745033, 52.3718889],
+                [4.8744518, 52.37206],
+                [4.8744399, 52.372201],
+                [4.8744832, 52.3723237],
+                [4.8745554, 52.3724494],
+                [4.8746452, 52.372562],
+                [4.874783, 52.3726898],
+                [4.8748307, 52.3727266],
+                [4.8751684, 52.3729238],
+                [4.8752354, 52.3729742],
+                [4.8752958, 52.3730353],
+                [4.8753841, 52.3731502],
+                [4.8754317, 52.3732351],
+                [4.8754871, 52.37341],
+                [4.8674055, 52.3717966],
+            ]
+        ]
+    )
+
+    pol.is_valid
+
+    valid = make_valid()
+
+    print(valid)
+
+    polygon = pd.DataFrame()
+    polygon["coordinates"] = jsonData["features"].apply(lambda row: json.dumps(make_valid(Polygon(row["geometry"]["coordinates"]))))
+    polygon["wijkcode"] = jsonData["features"].apply(lambda row: row["properties"]["Wijkcode"])
 
     cols_to_keep = ["Wijkcode", "LNG", "LAT"]
     data = raw[cols_to_keep].rename(columns={"LAT": "lat", "LNG": "lon", "Wijkcode": "wijkcode"})
 
-    return data
+    final = pd.merge(data, polygon, on="wijkcode")
+
+    final.head()
+    return final
