6ab47f74f7734de331e54605b81b4a35a0a46eeb
FaridullahKhorshid <faridullah.khorshid@gmail.com>
2022-06-16 12:14:13 +0200

    prediction

--- views/info.py
+++ views/info.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import base64
-from pptx import Presentation
-from io import BytesIO
-import streamlit.components.v1 as components
 
 
 def load_view():
@@ -26,25 +23,54 @@ def load_view():
         st.markdown(pdf_display, unsafe_allow_html=True)
 
     with col2:
-        from urllib.request import urlopen
 
         st.header("Download file's ")
-        st.download_button(label="📝 Sprint 1", data=open("data/Sprint1.pptx", "rb"), file_name="data/Sprint1.pptx")
-        st.download_button(label="📝 Sprint 2", data=open("data/Sprint2.pptx", "rb"), file_name="data/Sprint2.pptx")
-        st.download_button(label="📝 Sprint 3", data=open("data/Sprint2.pptx", "rb"), file_name="data/Sprint2.pptx")
-        st.download_button(label="📝 Sprint 4", data=open("data/Sprint4.pptx", "rb"), file_name="data/Sprint4.pptx")
-        st.download_button(label="📝 Business case", data=open("data/business_case.pdf", "rb"), file_name="data/business_case.pdf")
-        st.download_button(label="📝 Scrum process", data=open("data/scrum_pr.pdf", "rb"), file_name="data/scrum_pr.pdf")
+
         st.markdown(
-            f'<div class="row-widget stDownloadButton"> \
-                    <a href="https://github.com/FaridullahKhorshid/ProjectBigData/archive/refs/heads/master.zip"> \
-                     <button kind="primary" class="css-1cpxqw2">🗂 Github Zip</button> \
-                    </a> \
-                </div>',
+            f'<a href="https://github.com/FaridullahKhorshid/ProjectBigData/archive/refs/heads/master.zip"> \
+                <button kind="primary" class="custom_button">🗂 Github Zip</button> \
+            </a>',
             unsafe_allow_html=True,
         )
-        # st.download_button(
-        #     label="Github master zip",
-        #     data=urlopen("https://github.com/FaridullahKhorshid/ProjectBigData/archive/refs/heads/master.zip").read(),
-        #     file_name="top_team.zip",
-        # )
+
+        st.markdown(
+            f'<a href="https://github.com/FaridullahKhorshid/ProjectBigData/blob/master/data/business_case.pdf?raw=true"> \
+                <button kind="primary" class="custom_button">💼 Business case</button> \
+            </a>',
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            f'<a href="https://github.com/FaridullahKhorshid/ProjectBigData/blob/master/data/scrum_pr.pdf?raw=true"> \
+                <button kind="primary" class="custom_button">📊 Scrum process</button> \
+            </a>',
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            f'<a href="https://github.com/FaridullahKhorshid/ProjectBigData/blob/master/data/Sprint1.pptx?raw=true"> \
+                <button kind="primary" class="custom_button">📝 Sprint 1</button> \
+            </a>',
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            f'<a href="https://github.com/FaridullahKhorshid/ProjectBigData/blob/master/data/Sprint2.pptx?raw=true"> \
+                <button kind="primary" class="custom_button">📝 Sprint 2</button> \
+            </a>',
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            f'<a href="https://github.com/FaridullahKhorshid/ProjectBigData/blob/master/data/Sprint4.pptx?raw=true"> \
+                <button kind="primary" class="custom_button">📝 Sprint 4</button> \
+            </a>',
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            f'<a href="https://github.com/FaridullahKhorshid/ProjectBigData/blob/master/data/final_sprint.pptx?raw=true"> \
+                <button kind="primary" class="custom_button">📝 Final Sprint</button> \
+            </a>',
+            unsafe_allow_html=True,
+        )
